9f8145b32537cdc6a45ed4b29e4de4d17e29a11e
ILMAN <ilmanalisher896@gmail.com>
2024-12-13 12:52:08 +0500

    Serve Static File

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-const dbString ="mongodb://127.0.0.1:27017/NEW_LMS";
-// const liveDBString = process.env.DATABASE_STRING;
-mongoose.connect(dbString);
+// const dbString ="mongodb://127.0.0.1:27017/NEW_LMS";
+const liveDBString = process.env.DATABASE_STRING;
+mongoose.connect(liveDBString);
 
 
 
